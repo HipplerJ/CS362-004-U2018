48419e1f7be104d9428f2bb81259a7d6dec164cd
HipplerJ <32230179+HipplerJ@users.noreply.github.com>
2018-07-11 17:44:33 -0500

    Adding all 8 necessary files for unit testing and card testing.

---
+++ projects/hipplerj/dominion/cardtest4.c
@@ -0,0 +1,38 @@
+/*******************************************************************************
+** Author: James Hippler (HipplerJ)
+** Oregon State University
+** CS 362-400 (Spring 2018)
+** Software Engineering II
+**
+** Description: Assignment 3
+** Due: Sunday, June 22, 2018
+**
+** Filename: cardtest4.c
+**
+** OBJECTIVES:
+** Write unit tests for four Dominion cards implemented in dominion.c. Write
+** these tests so that they work whether a card is implemented inside cardEffect
+** or in its own function. These tests should be checked in as cardtest1.c,
+** cardtest2.c, cardtest3.c, and cardtest4.c. It is mandatory to test smithy
+** and adventurer card. (20 points)
+*******************************************************************************/
+
+/*******************************************************************************
+** PREPROCESSOR DIRECTIVES
+*******************************************************************************/
+
+/*******************************************************************************
+** GLOBAL CONSTANT DECLARATIONS
+*******************************************************************************/
+
+/*******************************************************************************
+** STRUCTURE DECLARATIONS
+*******************************************************************************/
+
+/*******************************************************************************
+** FUNCTION DECLARATIONS
+*******************************************************************************/
+
+/*******************************************************************************
+* Description: main function
+*******************************************************************************/
---
+++ projects/hipplerj/dominion/unittest2.c
@@ -0,0 +1,39 @@
+/*******************************************************************************
+** Author: James Hippler (HipplerJ)
+** Oregon State University
+** CS 362-400 (Spring 2018)
+** Software Engineering II
+**
+** Description: Assignment 3
+** Due: Sunday, June 22, 2018
+**
+** Filename: unittest2.c
+**
+** OBJECTIVES:
+** The primary goal of this assignment is to learn how to create unit tests.
+** Write unit tests for four functions (not card implementations, and not
+** cardEffect) in dominion.c “the refactored code you created for assignment-2”.
+** Check these tests in as unittest1.c,unittest2.c, unittest3.c, and
+** unittest4.c. At least two of these functions should be more than 5 lines of
+** code. (20 points)
+*******************************************************************************/
+
+/*******************************************************************************
+** PREPROCESSOR DIRECTIVES
+*******************************************************************************/
+
+/*******************************************************************************
+** GLOBAL CONSTANT DECLARATIONS
+*******************************************************************************/
+
+/*******************************************************************************
+** STRUCTURE DECLARATIONS
+*******************************************************************************/
+
+/*******************************************************************************
+** FUNCTION DECLARATIONS
+*******************************************************************************/
+
+/*******************************************************************************
+* Description: main function
+*******************************************************************************/
