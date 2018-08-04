a6c0a59c91fd466d1456b06faae2b031ef69cc30
James Hippler <hipplerj@oregonstate.edu>
2018-08-04 17:09:49 -0500

    Update Make File and Added a gitignore file to the project

--- projects/hipplerj/dominion/randomadventurer.c
+++ projects/hipplerj/dominion/randomadventurer.c
@@ -9,6 +9,32 @@
 **
 ** Filename: randomtestadventurer.c
 **
+** TEST CARD: adventurer
+** Reveal cards from your deck until you reveal 2 Treasure cards.  Put those
+** Treasure cards into your hand and discard the other revealed cards.
+**
+** GAME STATE STRUCTURE
+** struct gameState {
+**   int numPlayers; //number of players
+**   int supplyCount[treasure_map+1];  //this is the amount of a specific type of card given a specific number.
+**   int embargoTokens[treasure_map+1];
+**   int outpostPlayed;
+**   int outpostTurn;
+**   int whoseTurn;
+**   int phase;
+**   int numActions; // Starts at 1 each turn
+**   int coins; // Use as you see fit!
+**   int numBuys; // Starts at 1 each turn
+**   int hand[MAX_PLAYERS][MAX_HAND];
+**   int handCount[MAX_PLAYERS];
+**   int deck[MAX_PLAYERS][MAX_DECK];
+**   int deckCount[MAX_PLAYERS];
+**   int discard[MAX_PLAYERS][MAX_DECK];
+**   int discardCount[MAX_PLAYERS];
+**   int playedCards[MAX_DECK];
+**   int playedCardCount;
+** };
+**
 ** OBJECTIVES:
 ** Write an automated random test generator for three Dominion cards “use the
 ** refactored code you created for assignment-2”, one of them being the
@@ -26,6 +52,16 @@
 #include "rngs.h"                                                               // Include the rngs.h header file
 
 /*******************************************************************************
+** DEFINE GLOBAL VARIABLES
+*******************************************************************************/
+
+#define TEST_CARD_NAME "Adventurer"                                             // Define global variable for card name "smithy"
+#define TEST_FILE_NAME "randomtestadventurer.c"                                 // Define global variable for the file name used for testing
+#define KNRM "\x1B[0m"                                                          // Define global variable to change console color to Normal
+#define KRED "\x1B[31m"                                                         // Define global variable to change the console color to Red (Failed Tests)
+#define KGREEN "\x1B[32m"                                                       // Define global variable to change the console color to Green (Passed Tests)
+
+/*******************************************************************************
 ** PREPROCESSOR DIRECTIVES
 *******************************************************************************/
 
@@ -40,11 +76,32 @@
 ** FUNCTION DECLARATIONS
 *******************************************************************************/
 
+void print_header();
+void adventurer_rand_test();
+
+/*******************************************************************************
+* Description: main function
+*******************************************************************************/
+
 int main(int argc, char const *argv[]) {
-  printf("Hello World!\n");
+  print_header();
+  adventurer_rand_test();
   return 0;
 }
 
-void smithy_rand_test(){
+/*******************************************************************************
+* Description: print_header function
+*******************************************************************************/
+
+void print_header(){
+  printf("---------- Random Card Test: %s ----------\n", TEST_CARD_NAME);
+  printf("==> TEST FILE: %s\n", TEST_FILE_NAME);
+}
+
+/*******************************************************************************
+* Description: adventurer_rand_test function
+*******************************************************************************/
+
+void adventurer_rand_test(){
   printf("In the smithy random test\n");
 }
